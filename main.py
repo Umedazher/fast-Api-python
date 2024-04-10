8aeab0e36cfe241e1d1465b8de9174f25d9d0d2a
Umedazher <umaidazher@gmail.com>
2024-04-10 22:26:37 +0530

    port changes for deployement

--- main.py
+++ main.py
@@ -637,4 +637,5 @@ def add_friend_to_connections(data, userid):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    # uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), reload=True)
